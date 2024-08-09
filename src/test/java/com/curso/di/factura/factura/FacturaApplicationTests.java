fb0275468a0e609aa621fce7dc7dffdec7918c49
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-08-09 10:06:21 -0300

    commit inicial

---
+++ src/test/java/com/curso/di/factura/factura/FacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.di.factura.factura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
